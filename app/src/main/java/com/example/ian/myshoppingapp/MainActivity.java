3191d336d837b61e50d44d3bdcb05c9ed8cba38c
Ian <152772a@mymail.nyp.edu.sg>
2016-11-17 21:12:00 +0800

    i

--- app/src/main/java/com/example/ian/myshoppingapp/MainActivity.java
+++ app/src/main/java/com/example/ian/myshoppingapp/MainActivity.java
@@ -5,13 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView recentActTv;
+    EditText usernameEt,passwordET;
+    Button loginBT;
+    final int SHOP_ACTIVITY_RESULT_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recentActTv = (TextView) findViewById(R.id.recentActTv);
+        usernameEt= (EditText) findViewById(R.id.userNameET);
+        passwordET= (EditText) findViewById(R.id.passwordET);
+        loginBT= (Button) findViewById(R.id.loginBT);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
